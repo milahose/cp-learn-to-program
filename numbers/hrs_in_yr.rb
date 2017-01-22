5f2206ebd6f8caeb102e015851d83253cd2f5d5e
Mila Hose <mila.hose@gmail.com>
2017-01-21 21:38:48 -0800

    add hours in year calculation

---
+++ numbers/hrs_in_yr.rb
@@ -0,0 +1,11 @@
+# How many hours in a year?
+# 24 hours in a day
+# 7 days in a week 
+# 52 weeks in a year
+
+hrs_in_yr = 24 * 7 * 52
+
+puts "How many hours are in a year?"
+
+puts "There are 24 hours in a day, 7 days in a week and 52 weeks in a year, which equals #{hrs_in_yr} hours in a year!"
+
