2c822dd134b36395689d144da60ef9752cfd6056
Mila Hose <mila.hose@gmail.com>
2017-01-21 21:54:15 -0800

    add time up to hours

---
+++ numbers/how_old_am_i.rb
@@ -0,0 +1,13 @@
+# If I am 1,246 million seconds old, how old am i? 
+
+puts "If I am 1,246 million seconds old, how old am i?"
+
+puts "1,246,000,000 seconds dived by 60 seconds equals #{1246000000.0/60}."
+
+seconds_to_min = 1246000000.0/60
+
+puts "Now, #{seconds_to_min} mins dived by 24 hours equals #{seconds_to_min/24}"
+
+age_in_min = seconds_to_min/24
+
+puts "If I am #{age_in_min} minutes old"
